0bd0ee745aadfcd973e73049f3c280d398c0b1a8
Daniel Esteban <danideap1@gmail.com>
2021-06-13 19:56:43 -0500

    Practica 1

---
+++ POOP1/POOP1.java
@@ -0,0 +1,29 @@
+
+package POOP1;
+
+/**
+ *
+ * @author DanielEstebanAlcantara 
+ */
+public class POOP1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+        
+        
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
