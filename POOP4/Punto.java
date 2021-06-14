76224700ccf32fdc2c08e17a60ba21ad0cc868d1
Daniel Esteban <danideap1@gmail.com>
2021-06-13 19:57:12 -0500

    Practica 4

---
+++ POOP4/Punto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POOP4;
+
+/**
+ *
+ * @author Propietario
+ */
+public class Punto {
+    int x;
+    int y;
+
+    public Punto() {
+    }
+
+    
+    public Punto(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+           
+    public void imprimePunto(){
+        System.out.println("Coordenadas del punto x= "+x+" y= "+y);
+    }
+}
