1f6f60ae1667a4dae385e4c0d8670f6c7b675250
Daniel Esteban <danideap1@gmail.com>
2021-06-13 19:56:53 -0500

    Practica 2

---
+++ POOP2/ejemplos.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POOP2;
+
+/**
+ *
+ * @author Propietario
+ */
+public class ejemplos {
+    public static void main(String[] args){
+        
+        
+        boolean variable1;
+        char variable2;
+        char variable2_2 = 'c';
+        byte variable3;
+        short variable4;
+        short variable4_4 = 15;
+        int variable5;
+        int variable5_5 = 250;
+        float variable6;
+        float variable6_6 = 15.05;
+        
+        
+        
+    }
+}
