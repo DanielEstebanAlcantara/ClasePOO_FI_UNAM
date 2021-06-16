499002ff2e37ec0b3c3d34a8a262f09f450c3bd1
Daniel Esteban <danideap1@gmail.com>
2021-06-16 12:58:58 -0500

    Practica6

---
+++ POOP6IDE/POOP6IDE.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POOP6IDE;
+
+/**
+ *
+ * @author Propietario
+ */
+
+//import java.util.Random;
+
+public class POOP6IDE {
+    public static void main(String[] args){
+        //Random numero = new Random();
+        java.util.Random numero = new java.util.Random();
+        System.out.println(numero.nextInt(100));
+    }
+    
+}
---
+++ POOP6TERMINAR/MX/FI/POO/MiClase.java
@@ -0,0 +1,9 @@
+
+package POOP6TERMINAR.MX.FI.POO;
+ 
+
+public class MiClase{
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde clase Terminal");
+    }
+}
